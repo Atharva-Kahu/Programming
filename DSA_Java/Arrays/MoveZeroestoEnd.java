0fd93c331ab05bfdde10c0650513a9e9568df042
Atharva Kahu <kahuatharva@gmail.com>
2025-06-17 20:34:56 -0500

    third

---
+++ DSA_Java/Arrays/MoveZeroestoEnd.java
@@ -0,0 +1,41 @@
+public class MoveZeroestoEnd {
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3,0,0,0,2,0,3};
+        int n = arr1.length;
+        int ans[] = MoveZero(arr1,n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    private static int[] MoveZero(int[] arr1, int n) {
+        // TODO Auto-generated method stub
+        int j = -1;
+        for(int i=0;i<n;i++){
+            if(arr1[i]==0){
+                j = i;
+                break;
+            }
+        }
+        if (j == -1){
+            return arr1;
+
+        } 
+
+        for (int i = j+1; i <n; i++){
+            if(arr1[i]!=0){
+                int temp = arr1[i];
+                arr1[i] = arr1[j];
+                arr1[j] = temp;
+                j +=1;
+            }
+
+
+        }
+        return arr1;
+
+        
+    }
+    
+}
