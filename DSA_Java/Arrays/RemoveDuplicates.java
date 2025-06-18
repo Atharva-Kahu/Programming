695892193dd304e06b40940b5de38bc0ed01dbdb
Atharva Kahu <kahuatharva@gmail.com>
2025-06-18 01:06:21 -0500

    new_files

---
+++ DSA_Java/Arrays/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+public class RemoveDuplicates {
+    
+    public static void main(String[] args) {
+        int arr1[] = {1,1,2,3,3,3,4,4,5,5,7};
+        int n = arr1.length;
+        int arr2[] = Duplicates(arr1,n);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+    private static int[] Duplicates(int[] arr1, int n) {
+        int i = 0;
+        for(int j=1; j<n; j++){
+            if(arr1[j] != arr1[i]){
+                arr1[i+1] = arr1[j];
+                i+=1;
+
+            }          
+        }
+        return arr1;
+    }
+}
---
+++ DSA_Java/Arrays/RotateArrayElements.java
@@ -0,0 +1,46 @@
+public class RotateArrayElements {
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3,4,5,6,7};
+        int n = arr1.length;
+        int k = 2;
+        String side = "left";
+        int pos=0;
+        if (side=="left"){
+            pos = 0;
+        }
+        else if (side == "right"){
+            pos = -1;
+
+        }
+
+        int fin[] = LeftorRight(arr1,n,k);
+        for (int i = 0; i < fin.length; i++) {
+            System.out.print(fin[i] + " ");
+        }
+        System.out.println("");
+
+    }
+
+    static int[] LeftorRight(int[] arr1, int n, int k) {
+        int j =0;
+        int[] temp = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = arr1[i];
+        }
+
+        for (int i=j; i<n;i++){
+            arr1[i] = arr1[k+1];
+            k += 1;
+        }
+
+        for (int i=0;i<temp.length;i++){
+            int t = n-k;
+            arr1[t] = temp[i];
+            t+=1;
+
+        }
+
+        return temp;
+    }
+}
