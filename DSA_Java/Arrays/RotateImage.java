758dbc65de9afbce12aa0d10bc88e725e5e54fcb
Atharva Kahu <kahuatharva@gmail.com>
2025-06-20 13:37:13 -0500

    2D_Arrays

---
+++ DSA_Java/Arrays/RotateImage.java
@@ -0,0 +1,34 @@
+
+
+public class RotateImage {
+    public static void main(String[] args) {
+        int[][] arr1 = {
+                        {12, 34, 7},
+                        {45, 22, 9},
+                        {3, 18, 50}};
+        ;
+        int[][] ans = rotate(arr1);
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < ans.length; j++) {
+                    System.out.print(ans[i][j] + " ");
+                }}
+
+
+
+    }
+
+    // Brute Force
+
+    private static int[][] rotate(int[][] arr1) {
+        int n = arr1.length;
+        int rotated[][] = new int[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0; j<n;j++){
+                rotated[j][n-1-i] = arr1[i][j];
+            }
+        }
+        return rotated;
+    }
+    
+}
---
+++ DSA_Java/Arrays/StockBuySell.java
@@ -0,0 +1,41 @@
+public class StockBuySell {
+    public static void main(String[] args) {
+        int arr1[] = {20,50,10,30};
+
+        int profit = BuySell(arr1);
+        System.out.println(profit);
+
+        int profit1 = maxProfit(arr1);
+        System.out.println(profit1);
+    }
+// Brute Force
+    static int BuySell(int[] arr1) {
+        int n = arr1.length;
+        int profit = 0;
+        int temp = 0;
+        // TODO Auto-generated method stub
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(i<j){
+                    temp = arr1[j] - arr1[i];
+                    if(temp>profit){
+                        profit = temp;
+                    }
+                    
+                }
+            }
+
+        }
+        return profit;
+    }
+// Complex    
+    static int maxProfit(int[] arr) {
+    int maxPro = 0;
+    int minPrice = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+        minPrice = Math.min(minPrice, arr[i]);
+        maxPro = Math.max(maxPro, arr[i] - minPrice);
+    }
+    return maxPro;
+    }
+}
